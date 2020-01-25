a3194bac1b708c677b75024710fe58bbe9b1fdaf
nicolsss <nicole.gq@hotmail.com>
2020-01-25 17:33:30 -0500

    Trabajando en vistas de Productos/cotizacion

--- ProyectoDS/src/modelo/Venta.java
+++ ProyectoDS/src/modelo/Venta.java
@@ -1,5 +1,9 @@
 package modelo;
 
+import java.sql.ResultSet;
+import java.sql.Statement;
+import proyectods.ProyectoDS;
+
 /**
  *
  * @author Camilo
@@ -8,7 +12,64 @@ package modelo;
 public class Venta{
     private int cantidad;
     private Producto producto;
+    private String tipo;
+    private int id;
+    
+    public Venta(int cantidad, Producto producto, String tipo) {
+        this.cantidad = cantidad;
+        this.producto = producto;
+        this.tipo = tipo;
+        setVentaId();
+    }
 
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+    
+    private void setVentaId(){
+        switch(tipo){
+            case "producto":
+                    
+                String query = "SELECT MAX(id_detalle) From DetalleProducto";
+                try{
+                    Statement st = ProyectoDS.cdb.createStatement();
+                    ResultSet rs = st.executeQuery(query); 
+                    id = Integer.parseInt(rs.getString("id_detalle"))+1;
+                }catch(Exception e){
+                    System.out.println("Problemas en la Query, "+e);
+                }
+                break;
+            case "servicio":
+                                    
+                String query2 = "SELECT MAX(id_detalle) From DetalleServicio";
+                try{
+                    Statement st = ProyectoDS.cdb.createStatement();
+                    ResultSet rs = st.executeQuery(query2); 
+                    id = Integer.parseInt(rs.getString("id_detalle"))+1;
+                }catch(Exception e){
+                    System.out.println("Problemas en la Query, "+e);
+                }
+                break;
+            
+        
+    }
+                
+            
+            
+        }
+    
 
 }
     
