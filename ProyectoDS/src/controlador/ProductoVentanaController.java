a325a43a7798d56bea995dd5b943b5f319042a78
CarlosXavier <cxcarvaj@espol.edu.ec>
2020-01-25 17:33:26 -0500

    Metodos en vista ProductoVenta

--- ProyectoDS/src/controlador/ProductoVentanaController.java
+++ ProyectoDS/src/controlador/ProductoVentanaController.java
@@ -6,13 +6,28 @@
 package controlador;
 
 import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.StackPane;
+import modelo.Producto;
+import modelo.Venta;
+import proyectods.ProyectoDS;
 
 /**
  * FXML Controller class
@@ -21,6 +36,33 @@ import javafx.scene.layout.StackPane;
  */
 public class ProductoVentanaController extends StackPane implements Initializable {
     private Main application;
+    @FXML
+    private TableView<Producto> tabla;
+    @FXML
+    private ComboBox<String> combobox;
+    private List<Producto> productos = new LinkedList<>();
+    private List<Venta> ventas = new LinkedList<>();
+    @FXML
+    private TextField searchArtTxtfield;
+    @FXML
+    private TextField cantidadTxtField;
+    @FXML
+    private Button colocarButton;
+    @FXML
+    private TableColumn<Producto, String> TableColumnNombre;
+    @FXML
+    private TableColumn<Producto, Integer> TableColumnStock;
+    @FXML
+    private TableColumn<Producto, Float> TableColumnPrecioI;
+    @FXML
+    private TableColumn<Producto, Float> TableColumnPrecioM;
+    @FXML
+    private TableColumn<Producto, String> TableColumnCategoria;
+    @FXML
+    private TableColumn<Producto, String> TableColumnDesc;
+    @FXML
+    private TableColumn<Producto, Integer> TableColumnid;
+
     /**
      * Initializes the controller class.
      * @param url
@@ -28,7 +70,12 @@ public class ProductoVentanaController extends StackPane implements Initializabl
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        combobox= new ComboBox<>();
+        searchArtTxtfield = new TextField();
+        cantidadTxtField = new TextField();
+        tabla = new TableView<>();
+        combobox.getItems().addAll("Nombre","Categoria","Descripción");
+
     }    
 
     @FXML
@@ -50,7 +97,35 @@ public class ProductoVentanaController extends StackPane implements Initializabl
     public void setApp(Main application){
         this.application = application;
     }
-    
-    
-    
+
+    @FXML
+    private void searchArticulo(ActionEvent event) {
+        String query = "SELECT * FROM Producto where"+combobox.getValue()+"like \"%{$"+searchArtTxtfield.getText()+"}%\" estado = 1;";
+        try {
+            Statement st = ProyectoDS.cdb.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            while (rs.next()) {
+                float stk = Float.parseFloat(rs.getString("stock"));
+                float prvM = Float.parseFloat(rs.getString("precio_porMayor"));
+                float prvI = Float.parseFloat(rs.getString("precio_individual"));
+                Producto prd = new Producto(rs.getString("nombre"), stk, prvI, prvM, rs.getString("categoria"), rs.getString("descripcion"));
+                prd.setId(rs.getString("id_producto"));
+                productos.add(prd);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductoVentanaController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+         tabla.setItems((ObservableList<Producto>) productos);
+         searchArtTxtfield.setText(null);
+    }    
+
+    @FXML
+    private void añadirArticulo(ActionEvent event) {
+        Producto prd = tabla.getSelectionModel().getSelectedItem();
+        //Venta vt = new Venta(Integer.parseInt(cantidadTxtField.getText()),prd,"Factura");
+        ProductoVentanaController.factura,getVentas().add(vt);
+        cantidadTxtField.setText(null);
+        
+        
+    }
 }
--- ProyectoDS/src/modelo/Factura.java
+++ ProyectoDS/src/modelo/Factura.java
@@ -1,5 +1,6 @@
 package modelo;
 
+import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.LinkedList;
 import java.util.List;
@@ -11,10 +12,51 @@ import proyectods.ProyectoDS;
  */
 
 public class Factura extends Documento{
-    private String numFactura;
+    private int numFactura;
     private boolean domicilio;
     private LinkedList<Venta> ventas;
     private StrategyPay metodoPago;
+
+    public Factura(int numFactura, boolean domicilio, LinkedList<Venta> ventas, StrategyPay metodoPago) {
+        this.numFactura = numFactura;
+        this.domicilio = domicilio;
+        this.ventas = ventas;
+        this.metodoPago = metodoPago;
+    }
+
+    public int getNumFactura() {
+        return numFactura;
+    }
+
+    public void setNumFactura(int numFactura) {
+        this.numFactura = numFactura;
+    }
+
+    public boolean isDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(boolean domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public LinkedList<Venta> getVentas() {
+        return ventas;
+    }
+
+    public void setVentas(LinkedList<Venta> ventas) {
+        this.ventas = ventas;
+    }
+
+    public StrategyPay getMetodoPago() {
+        return metodoPago;
+    }
+
+    public void setMetodoPago(StrategyPay metodoPago) {
+        this.metodoPago = metodoPago;
+    }
+    
+    
 
     @Override
     public void agregar() {
@@ -41,6 +83,18 @@ public class Factura extends Documento{
     @Override
     public void delete() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public void getFacturaID(){
+        String query = "Select MAX(numFactura) From Factura";
+        try{
+            Statement st = ProyectoDS.cdb.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            numFactura = Integer.parseInt(rs.getString("numFactura"))+1;
+        }catch(Exception ex){
+            System.out.println("Problemas en la query "+ ex);
+        }
+        
     }
     
     
