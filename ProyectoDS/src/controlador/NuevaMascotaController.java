d7a4bbd54008be0d0500e64c586ca20eb1b64255
abimaell95 <gcemas20@gmail.com>
2020-01-25 22:36:47 -0500

    f

--- ProyectoDS/src/controlador/NuevaMascotaController.java
+++ ProyectoDS/src/controlador/NuevaMascotaController.java
@@ -6,13 +6,21 @@
 package controlador;
 
 import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
+import modelo.Cliente;
+import modelo.Mascota;
 
 /**
  * FXML Controller class
@@ -21,6 +29,22 @@ import javafx.scene.layout.StackPane;
  */
 public class NuevaMascotaController extends StackPane implements Initializable {
     private Main application;
+    @FXML
+    private ComboBox<String> comboSize;
+    @FXML
+    private Button addButton;
+    @FXML
+    private TextField nombre;
+    @FXML
+    private TextField tipo;
+    @FXML
+    private TextField raza;
+    @FXML
+    private TextField peso;
+    @FXML
+    private TextField altura;
+    @FXML
+    private TextField edad;
     /**
      * Initializes the controller class.
      * @param url
@@ -28,7 +52,8 @@ public class NuevaMascotaController extends StackPane implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        comboSize.getItems().removeAll(comboSize.getItems());
+        comboSize.getItems().addAll("Pequeña","Grande");
     }    
 
     @FXML
@@ -49,6 +74,37 @@ public class NuevaMascotaController extends StackPane implements Initializable {
     public void setApp(Main application){
         this.application = application;
     }
+
+    @FXML
+    private void addMascota(ActionEvent event) {
+        if(FacturarVentanaController.cliente!= null && comboSize.getSelectionModel().getSelectedItem() != null){
+            Cliente c = FacturarVentanaController.cliente;
+            String cd = c.getCedula();
+            String name = nombre.getText();
+            String race = raza.getText();
+            int age = Integer.parseInt(edad.getText());
+            float w = Float.parseFloat(peso.getText());
+            float h = Float.parseFloat(altura.getText());
+            String type = tipo.getText();
+            String size = comboSize.getSelectionModel().getSelectedItem();
+            String query = "INSERT INTO `mascota` (`nombre`, `raza`, `edad`, `peso`, `altura`, `tipo`, `dueño`, `tamaño`) VALUES ('"+name+"', '"+race+"', '"+age+"', '"+w+"', '"+h+"', '"+type+"', '"+cd+"', '"+size+"');";
+            try{
+                if(Main.cdb != null){
+                    Statement st = Main.cdb.createStatement();
+                    ResultSet rs = st.executeQuery(query);
+                }
+            }catch(SQLException ex){
+                System.out.println("no insert");
+            }
+            
+        }else{
+            System.out.println("No se añadió");
+        }
+        
+        
+    }
+    
+    
     
     
 }
