a1435f506ca3e6d7aca6edd7d847eec2594ebd0e
abimaell95 <gcemas20@gmail.com>
2020-01-25 21:55:14 -0500

    ventana entregas

--- ProyectoDS/src/controlador/EntregasVentanaController.java
+++ ProyectoDS/src/controlador/EntregasVentanaController.java
@@ -6,12 +6,18 @@
 package controlador;
 
 import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.StackPane;
 import modelo.Entrega;
 
@@ -34,6 +40,7 @@ public class EntregasVentanaController extends StackPane implements Initializabl
     private TableColumn<Entrega, String> repartidor;
     @FXML
     private TableColumn<Entrega, String> estado;
+    private ObservableList<Entrega> data;
     /**
      * Initializes the controller class.
      * @param url
@@ -41,7 +48,37 @@ public class EntregasVentanaController extends StackPane implements Initializabl
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
+        String query = "SELECT * FROM VEntrega;";
+        String eCliente = "";
+        String eDir = "";
+        String eDesc = "";
+        String eRepartidor = "";
+        String eEstado = "";
+        data = FXCollections.observableArrayList(); 
+        cliente.setCellValueFactory(new PropertyValueFactory<Entrega,String>("cliente"));
+        direccion.setCellValueFactory(new PropertyValueFactory<Entrega,String>("direccion"));
+        descripcion.setCellValueFactory(new PropertyValueFactory<Entrega,String>("descripcion"));
+        repartidor.setCellValueFactory(new PropertyValueFactory<Entrega,String>("repartidor"));
+        estado.setCellValueFactory(new PropertyValueFactory<Entrega,String>("estado"));
+        try{
+            if(Main.cdb != null){
+                Statement st = Main.cdb.createStatement();
+                ResultSet rs = st.executeQuery(query);
+                while (rs.next()) {
+                    eCliente = rs.getString("cliente");
+                    eDir = rs.getString("direccion");
+                    eDesc = rs.getString("tipoEntrega");
+                    eRepartidor = rs.getString("nombre_usuario");
+                    eEstado = rs.getString("estado");
+                    Entrega e = new Entrega(eCliente,eDir,eDesc,eRepartidor,eDesc);
+                    data.add(e);
+                }
+                entrega.setItems(data);
+            }
+            }catch(SQLException ex){
+                
+            }
+            
     }    
 
     @FXML
